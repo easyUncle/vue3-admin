e73564bb3513903451d3a72e4419e29d8c666c87
lidy4 <lidy4@allinpay.com>
2023-03-24 15:49:45 +0800

    fix(vue报错): 解决导入第三方插件时，导致vue被加载两次发生错误

--- vue.config.js
+++ vue.config.js
@@ -19,7 +19,17 @@ module.exports = {
     },
     before: require('./mock/mock-server.js')
   },
-
+  //error:onMounted is called when there is no active component instance to be associated with. Lifecycle injection APIs can only be used during execution of setup(). If you are using async setup(), make sure to register lifecycle hooks before the first await statement.
+  // 问题原因：因为 Vue 被加载了两次。一次来自主项目,一次来自通过npm link(或本地安装的依赖项)导入的项目。
+  // 发生情况，一般在引入第三方库时，该库也同样引入vue;造成vue被加载两次；所以两次加载vue造成的问题，可以尝试用这个方法解决
+  configureWebpack: {
+    resolve: {
+      symlinks: false,
+      alias: {
+        vue: path.resolve(`./node_modules/vue`)
+      }
+    }
+  },
   chainWebpack(config) {
     // 设置 svg-sprite-loader
     config.module.rule('svg').exclude.add(resolve('src/icons')).end()
